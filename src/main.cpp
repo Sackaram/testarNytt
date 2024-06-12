0db0e418b911c68cebb01616991e083e146c8001
viktor hansson <vicken_92@hotmail.com>
2024-06-12 03:48:43 +0200

    dogs?

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,19 @@ void test()
 int main()
 {
 
+    
     constexpr int test = 5;
+    std::string ok = "asdasd";
+
 
     for (size_t i = 0; i < test; i++)
     {
         std::cout << "Hello there!2" << std::endl;
     }
+
+
+    std::cout << "Changes were made.." << std::endl;
+    std::cout << "Hmmm..." << std::endl;
 
     return 0;
 }
